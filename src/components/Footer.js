ff35461b449b7a509f64aa908662db400f7ef06e
Bartek Musiał <bartlomiej.musial@hotmail.com>
2022-03-04 02:10:45 +0100

    WIP: Header component build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
     <footer className="flex flex-col items-center justify-center">
         <div className="w-auto px-2 md:px-5 py-5 mt-20
                 inline-flex justify-between md:justify-center items-center
-                bg-light rounded-t-lg text-dark font-inter"
+                bg-light-gray rounded-t-lg text-dark font-inter"
         >
             <PoweredByFlotiq />
             <p className="text-center text-xs md:text-base">
--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const Layout = ({ children, additionalClass = [] }) => (
-    <main className={['font-grotesque', ...additionalClass].join(' ')}>
+    <main className={['font-ubuntu bg-light-gray md:bg-white', ...additionalClass].join(' ')}>
+        <Header />
         {children}
         <Footer />
     </main>
