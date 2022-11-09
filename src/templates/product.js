3909a4c8154c4f10e4d3f6f6ebeba7daa8b68b39
jaceksekula1337 <110597769+jaceksekula1337@users.noreply.github.com>
2022-11-09 16:28:32 +0100

    Update product.js

--- src/templates/product.js
+++ src/templates/product.js
@@ -10,41 +10,29 @@ import ReviewsSection from '../sections/ReviewsSection';
 import Avatar from '../assets/avatar.jpg';
 
 const reviews = [
-    {
-        review: 'The best store in our town! plants are always in good condition.',
-        author: 'Joe Jonas'
-    },
-    {
-        review: 'I ordered 3 packs of roses last friday and they all came so fast and fresh',
-        author: 'Kate Smith'
-    },
-    {
-        review: 'The staff was so sweet they helped me decorate my room with plants!',
-        author: 'John Doe'
-    },
-    {
-        review: 'The best store in our town! plants are always in good condition.',
-        author: 'Jane Doe'
-    },
-    {
-        review: 'I ordered 3 packs of roses last friday and they all came so fast and fresh',
-        author: 'John Kovalski'
-    },
-    {
-        review: 'The staff was so sweet they helped me decorate my room with plants!',
-        author: 'Joe Novak'
-    },
+    { review: 'The best store in our town! plants are always in good condition.',
+        author: 'Joe Jonas' },
+    { review: 'I ordered 3 packs of roses last friday and they all came so fast and fresh',
+        author: 'Kate Smith' },
+    { review: 'The staff was so sweet they helped me decorate my room with plants!',
+        author: 'John Doe' },
+    { review: 'The best store in our town! plants are always in good condition.',
+        author: 'Jane Doe' },
+    { review: 'I ordered 3 packs of roses last friday and they all came so fast and fresh',
+        author: 'John Kovalski' },
+    { review: 'The staff was so sweet they helped me decorate my room with plants!',
+        author: 'Joe Novak' },
 ];
 
 const buttonLabel = 'Add to cart';
-const sizes = [
-    { name: 'XS', current: false },
-    { name: 'S', current: false },
-    { name: 'M', current: false },
-    { name: 'L', current: true },
-    { name: 'XL', current: false },
-    { name: 'XXL', current: false },
-];
+// const sizes = [
+//     { name: 'XS', current: false },
+//     { name: 'S', current: false },
+//     { name: 'M', current: false },
+//     { name: 'L', current: true },
+//     { name: 'XL', current: false },
+//     { name: 'XXL', current: false },
+// ];
 
 /**
  * Content of example page
@@ -91,27 +79,27 @@ const ExamplePage = ({ data }) => {
                         {product.description}
                     </Paragraph>
                     <div className="flex flex-col w-full md:w-1/2">
-                        <div className="flex items-center justify-between text-xl font-light border-b border-gray/30 
-                         py-7 uppercase"
-                        >
-                            Size
-                            <div>
-                                <label htmlFor="categoryTabs" className="sr-only">
-                                    Select a tab
-                                </label>
-                                {/* /! Use an "onChange" listener to redirect the user to the selected tab URL. !/ */}
-                                <select
-                                    id="tabs"
-                                    name="tabs"
-                                    className="block w-full pl-7 pr-14 py-1 text-lg !font-light bg-light-gray"
-                                    defaultValue={sizes.find((tab) => tab.current).name}
-                                >
-                                    {sizes.map((tab) => (
-                                        <option key={tab.name}>{tab.name}</option>
-                                    ))}
-                                </select>
-                            </div>
-                        </div>
+                        {/* <div className="flex items-center justify-between text-xl font-light border-b border-gray/30 */}
+                        {/* py-7 uppercase" */}
+                        {/* > */}
+                        {/*    Size */}
+                        {/*    <div> */}
+                        {/*        <label htmlFor="categoryTabs" className="sr-only"> */}
+                        {/*            Select a tab */}
+                        {/*        </label> */}
+                        {/*        /!* Use an "onChange" listener to redirect the user to the selected tab URL. *!/ */}
+                        {/*        <select */}
+                        {/*            id="tabs" */}
+                        {/*            name="tabs" */}
+                        {/*            className="block w-full pl-7 pr-14 py-1 text-lg !font-light bg-light-gray" */}
+                        {/*            defaultValue={sizes.find((tab) => tab.current).name} */}
+                        {/*        > */}
+                        {/*            {sizes.map((tab) => ( */}
+                        {/*                <option key={tab.name}>{tab.name}</option> */}
+                        {/*            ))} */}
+                        {/*        </select> */}
+                        {/*    </div> */}
+                        {/* </div> */}
                         <div className="flex items-center justify-between text-xl font-light py-7 uppercase">
                             Quantity
                             <div className="flex items-center md:items-stretch ml-8 font-roboto bg-light-gray ">
