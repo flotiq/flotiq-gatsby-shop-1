8ff3b4ce22f065901eb46cf075d4f1c9e112833a
jaceksekula1337 <110597769+jaceksekula1337@users.noreply.github.com>
2022-11-25 08:27:36 +0100

    flotiq image to gatsby image

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Header } from 'flotiq-components-react';
-import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image";
 
 const linkedinPath = 'M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.4'
     + '8-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.'
